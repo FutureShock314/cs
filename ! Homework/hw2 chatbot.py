dfc837b810ab3a860db68e8ef2058722a11dce3f
FutureShock314 <117740939+FutureShock314@users.noreply.github.com>
2023-10-01 19:00:37 +0000

    HW2 begins

---
+++ ! Homework/hw2 chatbot.py
@@ -0,0 +1,40 @@
+from time import sleep
+# import math
+
+print('\nGood day, old chap!')
+sleep(1)
+
+print('My name\'s Sir Charles, Duke of Essex, and thou shalt address me as such, peasant')
+sleep(5)
+
+name = input(
+    'Now that the important pleasantries are over, it would be rude of me to not ask your name?\n'
+    )
+sleep(1)
+
+print(f'Sorry, did you say {name}?')
+sleep(.5)
+print('Yes??')
+sleep(.5)
+print('Haha, forgive me, but that name is utterly hilarious!')
+sleep(0.2)
+print('How can you possibly live with such a ridiculous name?')
+for i in range(5):
+    sleep(i)
+    print('Ha')
+
+sleep(3)
+print('Well, that was awkward')
+sleep(0.5)
+print('Anyways, now that that\'s over, it would also be rude of me to not ask how you are?')
+input()
+
+print('yes, yes, good and all that jazz, I\'m feeling rather splendid today, thanks for asking')
+sleep(3)
+print('so, onto the important stuff.')
+sleep(2)
+print('The Stock Market')
+sleep(4)
+print('what, you don\'t follow the stock market?')
+sleep(3)
+print('Ugh, peasants')
