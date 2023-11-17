f476a21847358d44a88c30d7497c7722e8bf1164
FutureShock314 <117740939+FutureShock314@users.noreply.github.com>
2023-11-17 10:40:45 +0000

    18

--- 18--list-methods--17-11-23/main.js
+++ 18--list-methods--17-11-23/main.js
@@ -1,6 +1,13 @@
 suits = ["♥", "♦", "♣", "♠"];
 deck = [];
 
-for (var i = 1; i == length(suits); i ++) {
+sVals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'J', 'Q', 'K']
+
+for (var i = 1; i < suits.length; i++) {
     console.log(suits[i]);
+    for (var j = 1; j < sVals.length; j++) {
+        deck.push(`${suits[i]}${sVals[j]}`)
+    };
 };
+
+console.log(deck)
