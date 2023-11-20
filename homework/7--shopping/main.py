2fe6a38a8279b8baebf7f87a872f50d2b4673618
FutureShock314 <ayaan.waqas@outlook.com>
2023-11-20 20:13:16 +0000

    almost lost these changes, thank nvim for saving them: finish help, add add and list

--- homework/7--shopping/main.py
+++ homework/7--shopping/main.py
@@ -5,10 +5,24 @@ with open('./help.json', 'r') as f:
 
 # TODO: use a list to randomly print a different entry to the cmd prompt
 
+shoppingList = []
+
 def cmdHelp(cmd: list):
     if len(cmd) == 1:
         for cmdName in helpJson.keys():
             print(f'{cmdName}:\n\tsyntax:\t  {helpJson[cmdName]["syntax"]}\n\tdesc:\t  {helpJson[cmdName]["desc"]}\n')
+    elif cmd[1] in helpJson.keys():
+        print(f'Help for {cmd[1]}:\n\tsyntax:\t  {helpJson[cmd[1]]["syntax"]}\n\tdesc:\t  {helpJson[cmd[1]]["desc"]}\n')
+    else:
+        print(f'No command \'{cmd[1]}\' found')
+
+def cmdAdd(cmd: list):
+    list = shoppingList
+    try:
+        list.append(cmd[1])
+        print(f'Successfully added {cmd[1]} to the list!\n')
+    except:
+        print(f'Failed to add {cmd[1]} to the list. Not sure how you did this\n')
 
 os.system('clear')
 
@@ -32,10 +46,21 @@ computerNum = random.randint(100, 999)
 command = ''
 
 while command != 'exit':
-    command = input(f'User@SomeMachine{computerNum}$ ')
-    if command == 'exit':
+    command = input(f'User@SomeMachine{computerNum}$ ').split(' ')
+    if command[0] == 'exit':
         os.system('clear')
         quit()
 
-    if command == 'help':
-        cmdHelp(command.split(' '))
+    if command[0] == 'help':
+        print()
+        cmdHelp(command)
+
+    if command[0] == 'add':
+        print()
+        cmdAdd(command)
+
+    if command[0] == 'list':
+        print()
+        for i in range(len(shoppingList)):
+            print(f'{i + 1}. {shoppingList[i]}')
+        print()
