131fb07b25af9e7e5a9e73a227311c18ae8bd19a
FutureShock314 <117740939+FutureShock314@users.noreply.github.com>
2023-11-24 08:59:42 +0000

    basic auth add

--- projects/term-shell/main.py
+++ projects/term-shell/main.py
@@ -1,9 +1,12 @@
 import json
+import userAuth
 
-with open('./users.json', 'r') as f:
-    users = json.load(f)
-users["nathan"] = "password"
-users = json.dumps(users, indent=4)
+# with open('./users.json', 'r') as f:
+#     users = json.load(f)
+# users["nathan"] = "password"
+# users = json.dumps(users, indent=4)
 
-with open('./users.json', 'w') as f:
-    f.write(users)
+# with open('./users.json', 'w') as f:
+#     f.write(users)
+
+user-auth.auth()
