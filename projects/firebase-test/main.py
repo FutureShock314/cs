b1556a6c28595fcba74c2595a71b7d790f54cf51
FutureShock314 <117740939+FutureShock314@users.noreply.github.com>
2024-01-09 09:04:57 +0000

    db test

--- projects/firebase-test/main.py
+++ projects/firebase-test/main.py
@@ -6,8 +6,12 @@ default_app = firebase_admin.initialize_app(cred_object, {
 	'databaseURL': 'https://cs-firebase-test-7f9d5-default-rtdb.europe-west1.firebasedatabase.app/'
 })
 
-ref = firebase_admin.db.reference('/')
+ref = db.reference('/')
 
 with open('test.json', 'r') as f:
 	test = json.load(f)
-ref.set(test)
+# ref.set(test)
+
+get = ref.get()
+
+print(get)
