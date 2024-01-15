d13e7707751f5d5cbc99a2f1fee50e33ea040b56
FutureShock314 <117740939+FutureShock314@users.noreply.github.com>
2024-01-15 12:05:13 +0000

    i worked it out put the BeautifulSoup(temp0 in a var

--- projects/webscraper-y10/main.py
+++ projects/webscraper-y10/main.py
@@ -12,3 +12,9 @@ day = soup.find('div', attrs = {'aria-labelledby': 'day0Header'})
 items_time = soup.find_all('th', id = lambda x: x and x.startswith('d0t'))
 times = [item.get_text().strip() for item in items_time]
 print(times)
+
+items_temp = soup.find_all('td', attrs = {'headers': lambda x: x and x.startswith('d0Temp d0t')})
+# temps = [BeautifulSoup(item, 'html.parser').find_all('div') for item in items_temp]
+temp0 = items_temp[0]
+temps = BeautifulSoup(temp0, 'html.parser').find('div')
+print(temps)
