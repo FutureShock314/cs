b631753f9b1f1230445b87301089fc6f1d0d673b
FutureShock314 <117740939+FutureShock314@users.noreply.github.com>
2024-01-24 09:58:51 +0000

    format fix, make main work w/ 25%

--- projects/webscraper-y10/main.py
+++ projects/webscraper-y10/main.py
@@ -1,5 +1,30 @@
 from auth.auth import auth
+from time import sleep as sl
+import random
 
-auth()
+from weather import fetchWeather
+from news import fetchNews
+
+# auth()
 
 ... # dont need to check for auth(), if it fails it always quits ( i think )
+
+possible = ['n', 'w', 'news', 'weather'] # makes the while loop easier
+input_   = input('What would you like to see, news or weather? [w/n/news/weather]\n>> ')
+
+while input_.lower() not in possible:
+    print('not an option mate, you can type n, w, news, or weather')
+    sl(1)
+    input_ = input('What would you like to see, news or weather? [w/n/news/weather]\n>> ')
+
+if input_.lower() in ['n', 'news']:
+    print('news')
+    if random.randint(1, 4) == 2:
+        fetchWeather() # 25% of weather if news
+    else:
+        fetchNews()
+else:
+    if random.randint(1, 4) == 4:
+        fetchNews() # 25% of news if weather
+    else:
+        fetchWeather()
