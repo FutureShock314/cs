574a044ab4532ca928339b4e2c7a451054344e8f
FutureShock314 <ayaan.waqas@outlook.com>
2024-01-26 19:04:22 +0000

    not sure but pycharm made me do it

--- projects/webscraper-y10/news.py
+++ projects/webscraper-y10/news.py
@@ -1,29 +1,33 @@
 import requests, pandas as pd, time
 from bs4 import BeautifulSoup
 
-def timer(func):
+
+def timer(func) -> any:
     def wrapper(*args):
         start = time.time()
         func()
         end = time.time()
         print(f'operation took {end - start} seconds')
+
     return wrapper
 
+
 @timer
-def fetchNews():
+def fetchNews() -> None:
     ...
     url = 'https://www.bbc.co.uk/'
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
 
     # print(r.content)
-    items_headlines = soup.find_all('p', attrs = { 'class': lambda x: x and 'PromoHeadline' in x })
+    items_headlines = soup.find_all('p', attrs={ 'class': lambda x: x and 'PromoHeadline' in x })
     text_headlines = [item.get_text() for item in items_headlines]
     headlines = [text_headlines[i] for i in range(9)]
     print(headlines)
 
     for headline in headlines:
         print(headline)
+
 
 if __name__ == '__main__':
     fetchNews()
