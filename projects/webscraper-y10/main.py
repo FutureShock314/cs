8ae7e1c03d0d1068d6b4c6944807e9415f33dfa2
FutureShock314 <117740939+FutureShock314@users.noreply.github.com>
2024-02-28 09:16:35 +0000

    building up saved data for some reason

--- projects/webscraper-y10/main.py
+++ projects/webscraper-y10/main.py
@@ -11,7 +11,7 @@ from news import fetchNews
 
 # print(sys.argv)
 
-... # dont need to check for auth(), if it fails it always quits ( i think )
+... # dont need to check for auth(), if it fails it always quits ( i think ) ( after rigorous testing i am slightly more sure )
 
 possible = ['n', 'w', 'news', 'weather'] # makes the while loop easier
 input_   = input('What would you like to see, news or weather? [w/n/news/weather]\n>> ')
