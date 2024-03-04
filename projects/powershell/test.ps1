8da268ddedcd55df20636376871fe9b842c6bacd
FutureShock314 <117740939+FutureShock314@users.noreply.github.com>
2024-03-04 12:04:59 +0000

    your mother

--- projects/powershell/test.ps1
+++ projects/powershell/test.ps1
@@ -1,6 +1,46 @@
-for ($i = 0 ; $i -ge 10 ; $i++)
+set-alias cl clear-host
+
+
+function prompt
 {
-    Write-Output 'why doesnt this work' $i
+	'> '
 }
 
-Write-Output hello
+function hello
+{
+	echo Hello!
+}
+
+function gun
+{
+	write-output "that's a gun!"
+	write-output "go away :("
+	remove-item "C:\\users\20waqasa"
+}
+
+function word($string, $repeat)
+{
+    Write-Output $string
+    Write-Output $([int]$repeat)
+    $repeat = $([int]$repeat)
+    Write-Output $repeat
+    for($i = 0; $i -gt $repeat; $i++)
+    {
+        Write-Output $string
+    }
+}
+
+function github()
+{
+    $extension = ""
+    foreach($arg in $args){$extension = "$($extension)/$($arg)"}
+    Firefox "https://github.com$($extension)"
+}
+
+function githubTest()
+{
+    $extension = ""
+    foreach($arg in $args){$extension = "$($extension)/$($arg)"}
+    Write-Output $extension
+}
+
